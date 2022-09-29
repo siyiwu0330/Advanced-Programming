ff18efa86f5aaf4f35c9f4a20ec7633c908ef148
Thyme <40969794+NanaKoori@users.noreply.github.com>
2022-09-30 02:45:08 +0800

    Update Assignment3

---
+++ Assignment3/hand_in/code/part2/.stack-work/dist/d53b6a14/build/autogen/Paths_boa.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -Wno-missing-safe-haskell-mode #-}
+module Paths_boa (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "D:\\Study\\AP\\Github\\Advanced-Programming\\Assignment3\\hand_in\\code\\part2\\.stack-work\\install\\82a24295\\bin"
+libdir     = "D:\\Study\\AP\\Github\\Advanced-Programming\\Assignment3\\hand_in\\code\\part2\\.stack-work\\install\\82a24295\\lib\\x86_64-windows-ghc-9.0.2\\boa-0.0.0-FfGMUxGz3Kj8TbKBI3lCVi"
+dynlibdir  = "D:\\Study\\AP\\Github\\Advanced-Programming\\Assignment3\\hand_in\\code\\part2\\.stack-work\\install\\82a24295\\lib\\x86_64-windows-ghc-9.0.2"
+datadir    = "D:\\Study\\AP\\Github\\Advanced-Programming\\Assignment3\\hand_in\\code\\part2\\.stack-work\\install\\82a24295\\share\\x86_64-windows-ghc-9.0.2\\boa-0.0.0"
+libexecdir = "D:\\Study\\AP\\Github\\Advanced-Programming\\Assignment3\\hand_in\\code\\part2\\.stack-work\\install\\82a24295\\libexec\\x86_64-windows-ghc-9.0.2\\boa-0.0.0"
+sysconfdir = "D:\\Study\\AP\\Github\\Advanced-Programming\\Assignment3\\hand_in\\code\\part2\\.stack-work\\install\\82a24295\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "boa_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "boa_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "boa_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "boa_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "boa_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "boa_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
--- Assignment3/hand_in/code/part2/src/BoaParser.hs
+++ Assignment3/hand_in/code/part2/src/BoaParser.hs
@@ -3,17 +3,14 @@
 module BoaParser (ParseError, parseString) where
 
 import BoaAST
-import Text.ParserCombinators.ReadP as RP
+import Text.ParserCombinators.ReadP
 import Data.Char
-import Control.Applicative
+import Control.Applicative ((<|>))
 -- add any other other imports you need
 
-type ParseError = String -- you may replace this
+type Parser a = ReadP a
 
-intNum :: Parser Exp
-intNum = token (do num <- digits
-                   return (Num (read num)))
-       where digits = many1(satisfy isDigit)
+type ParseError = String -- you may replace this
 
 space :: Parser Char
 space = satisfy isSpace
@@ -30,38 +27,13 @@ symbol = token . string
 schar :: Char -> Parser Char
 schar = token . char
 
--- Name    ::= Letter Letdigs 
--- Letdigs ::= Letter Letdigs | Digit Letdigs | ε 
--- Letter  ::= "A" | ... | "Z" | "a" | ... | "z" 
--- Digit   ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
-
--- LegalNum      ::= "-" Digits | Digits
--- Digits        ::= Digit | NoneZeroDigit Digits 
--- Digit         ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
--- NoneZeroDigit ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
-
--- Comment ::=
-
-
-name :: Parser String
-name = token (do c <- letter
-                 cs <- letdigs
-                 return (c:cs))
-     where letter = satisfy isLetter
-           letdigs = many (letter <|> num)
-           num = satisfy isDigit
-
-data Keyword = Keyword String
-
 reserved :: [String]
 reserved = ["None", "True", "False", "for", "if", "in", "not"]
 
-nameOrKeyword :: Parser (Either Keyword String)
-nameOrKeyword  = do n <- name
-                    if n `elem` reserved then return (Left(Keyword n))
-                    else return (Right n)
+data Keyword = Keyword String
 
-
+program :: Parser Program
+program = stmts
 
 -- Stmts     = (do Stmt
 --                 return ())
@@ -69,12 +41,28 @@ nameOrKeyword  = do n <- name
 --                 symbol ";"
 --                 Stmts
 --                 return ())
+stmts :: Parser [Stmt]
+stmts            =   do st <- stmt
+                        return [st]
+                <++ (do st <- stmt
+                        symbol ";"
+                        ss <- stmts
+                        return (st:ss))
+
 -- Stmt      = (do ident
 --                 symbol "="
 --                 Expr
 --                 return ())
 --         <|> (do Expr
 --                 return ())
+stmt :: Parser Stmt
+stmt              = (do id <- ident
+                        symbol "="
+                        er <- expr
+                        return (SDef id er))
+                <++ (do er <- expr
+                        return (SExp er))
+
 -- Expr      = (do numConst
 --                 return ())
 --         <|> (do stringConst
@@ -101,15 +89,21 @@ nameOrKeyword  = do n <- name
 --         <|> (do ident
 --                 symbol "("
 --                 Exprz
---                 symbol ")")
+--                 symbol ")"
+--                 return ())
 --         <|> (do symbol "["
 --                 Exprz
---                 symbol "]")
+--                 symbol "]"
+--                 return ())
 --         <|> (do symbol "["
 --                 Expr
 --                 ForClause
 --                 Clausez
---                 symbol "]")
+--                 symbol "]"
+--                 return ())
+expr :: Parser Exp
+expr              = oper
+
 -- Oper      = (do symbol "+"
 --                 return ())
 --         <|> (do symbol "-"
@@ -137,14 +131,129 @@ nameOrKeyword  = do n <- name
 --         <|> (do symbol "not"
 --                 symbol "in"
 --                 return ())
+oper :: Parser Exp
+oper              = (do e1 <- oper1
+                        symbol "=="
+                        e2 <- oper1
+                        return (Oper Eq e1 e2))
+                <++ (do e1 <- oper1
+                        symbol "<"
+                        e2 <- oper1
+                        return (Oper Less e1 e2))
+                <++ (do e1 <- oper1
+                        symbol ">"
+                        e2 <- oper1
+                        return (Oper Greater e1 e2))
+                <++ (do e1 <- oper1
+                        symbol "in"
+                        e2 <- oper1
+                        return (Oper In e1 e2))
+                <++ (do e1 <- oper1
+                        symbol "!="
+                        e2 <- oper1
+                        return (Not (Oper Eq e1 e2)))
+                <++ (do e1 <- oper1
+                        symbol "<="
+                        e2 <- oper1
+                        return (Not (Oper Greater e1 e2)))
+                <++ (do e1 <- oper1
+                        symbol ">="
+                        e2 <- oper1
+                        return (Not (Oper Less e1 e2)))
+                <++ (do e1 <- oper1
+                        symbol "not"
+                        symbol "in"
+                        e2 <- oper1
+                        return (Not (Oper In e1 e2)))
+                <++ oper1
+
+oper1 :: Parser Exp
+oper1             = (do e <- oper2
+                        oper1' e)
+                <++ oper2
+
+oper1' :: Exp -> Parser Exp
+oper1' inval      = (do symbol "+"
+                        e <- oper2
+                        return (Oper Plus inval e))
+                <++ (do symbol "-"
+                        e <- oper2
+                        return (Oper Minus inval e))
+                <++ return inval
+
+oper2 :: Parser Exp
+oper2             = (do e <- oper3
+                        oper2' e)
+                <++ oper3
+
+oper2' :: Exp -> Parser Exp
+oper2' inval      = (do symbol "*"
+                        e <- oper3
+                        return (Oper Times inval e))
+                <++ (do symbol "//"
+                        e <- oper3
+                        return (Oper Div inval e))
+                <++ (do symbol "%"
+                        e <- oper3
+                        return (Oper Mod inval e))
+                <++ return inval
+
+oper3 :: Parser Exp
+oper3             = (do num <- numConst
+                        return (Const (IntVal num)))
+                <++ (do str <- stringConst
+                        return (Const (StringVal str)))
+                <++ (do symbol "None"
+                        return (Const NoneVal))
+                <++ (do symbol "True"
+                        return (Const TrueVal))
+                <++ (do symbol "False"
+                        return (Const FalseVal))
+                <++ (do id <- ident
+                        return (Var id))
+                <++ (do symbol "not"
+                        er <- expr
+                        return (Not er))
+                <++ (do symbol "("
+                        er <- expr
+                        symbol ")"
+                        return er)
+                <++ (do id <- ident
+                        symbol "("
+                        ez <- exprz
+                        symbol ")"
+                        return (Call id ez))
+                <++ (do symbol "["
+                        ez <- exprz
+                        symbol "]"
+                        return (List ez))
+                <++ (do symbol "["
+                        er <- expr
+                        fc <- forClause
+                        cz <- clausez
+                        symbol "]"
+                        return (Compr er (fc:cz)))
+
 -- ForClause =  do symbol "for"
 --                 ident
 --                 symbol "in"
 --                 Expr
 --                 return ()
+forClause :: Parser CClause
+forClause         =  do symbol "for"
+                        id <- ident
+                        symbol "in"
+                        er <- expr
+                        return (CCFor id er)
+
 -- IfClause  =  do symbol "if"
 --                 Expr
 --                 return ()
+ifClause :: Parser CClause
+ifClause          =  do symbol "if"
+                        er <- expr
+                        return (CCIf er)
+
 -- Clausez   = (return ())
 --         <|> (do ForClause
 --                 Clausez
@@ -152,30 +261,96 @@ nameOrKeyword  = do n <- name
 --         <|> (do IfClause
 --                 Clausez
 --                 return ())
+clausez :: Parser [CClause]
+clausez           = (return [])
+                <++ (do fc <- forClause
+                        cz <- clausez
+                        return (fc:cz))
+                <++ (do ic <- ifClause
+                        cz <- clausez
+                        return (ic:cz))
+
 -- Exprz     = (return ())
 --         <|> (do Exprs
 --                 return ())
+exprz :: Parser [Exp]
+exprz             = (return [])
+                <++ (do es <- exprs
+                        return es)
+
 -- Exprs     = (do Expr
 --                 return ())
 --         <|> (do Expr
 --                 symbol ","
 --                 Exprs
 --                 return ())
+exprs :: Parser [Exp]
+exprs             = (do er <- expr
+                        return [er])
+                <++ (do er <- expr
+                        symbol ","
+                        es <- exprs
+                        return (er:es))
 
--- ident        ::= String
--- numConst     ::= "-" digits | digits
--- stringConst  ::= "'" catchString "'"
+-- Name    ::= Letter Letdigs 
+-- Letdigs ::= Letter Letdigs | Digit Letdigs | ε 
+-- Letter  ::= "A" | ... | "Z" | "a" | ... | "z" 
+-- Digit   ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
+name :: Parser String
+name = token (do c <- letter
+                 cs <- letdigs
+                 return (c:cs))
+     where letter = satisfy isLetter
+           letdigs = many (letter <|> num)
+           num = satisfy isDigit
+nameOrKeyword :: Parser (Either Keyword String)
+nameOrKeyword  = do n <- name
+                    if n `elem` reserved then return (Left(Keyword n))
+                    else return (Right n)
+ident :: Parser String
+ident             =  token  (do nok <- nameOrKeyword
+                                case nok of
+                                    (Right id) -> return id
+                                    (Left _)  -> fail "It's a Keyword")
 
--- ident     = num
--- numConst  = (do symbol "-"
---                 digits
---                 return ())
---         <|> (do digits
---                 return ())
--- stringConst=(do symbol "'"
---                 catchString
---                 symbol "'")
+-- LegalNum      ::= "-" Digits | Digits
+-- Digits        ::= Digit | NoneZeroDigit Digits | Digit Digits 
+-- Digit         ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
+-- NoneZeroDigit ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" 
+-- numConst :: Parser Int
+-- numConst          = token ( (do ds <- digits
+--                                 nzd <- noneZeroDigit
+--                                 return $ read $ [nzd] ++ ds)
+--                         <++ (do char '-'
+--                                 ds <- digits
+--                                 nzd <- noneZeroDigit
+--                                 return $ read $ "-" ++ [nzd] ++ ds))
+--                     where digits = many (satisfy isDigit);
+--                           noneZeroDigit = satisfy (\x -> x `elem` ['1'..'9'])
+numConst :: Parser Int
+numConst          =  do x <- option ' ' (char '-')
+                        xs <- munch1 isDigit
+                        if (head xs == '0') && (length xs > 1)
+                            then fail "Num Error"
+                        else
+                            return $ read $ (x:xs)
+
+stringConst :: Parser String
+stringConst       =  do schar '\''
+                        c <- isString
+                        char '\''
+                        return c
+                where isString = many (satisfy isPrint)
+
+                    
+-- stringConst       = token (do cs <- letters
+--                               return cs)
+--                     where letters = many (satisfy isPrint)
+
+
 
 
 parseString :: String -> Either ParseError Program
-parseString = undefined  -- define this
+parseString str = case (readP_to_S program) str of
+  [] -> Left "Parsing error"
+  _  -> Right (fst (last ((readP_to_S program) str)))
